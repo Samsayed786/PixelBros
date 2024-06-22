14dca8cff022468b43f6ef1f14b263f6daa36ae8
Sayed Akid Hussain <samsayed07866@gmail.com>
2024-06-21 18:04:11 -0600

    Upload Project

---
+++ Assets/Game/Scripts/CameraController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float maxVerticalAngle;
+    public Transform body;
+    private RaycastHit hit;
+    private float _mouseVerticalValue;
+    private Vector3 cam_offset;
+    private float MouseVerticalValue
+    {
+        get => _mouseVerticalValue;
+        set
+        {
+            if (value == 0) return;
+            
+            float verticalAngle = _mouseVerticalValue + value;
+            verticalAngle = Mathf.Clamp(verticalAngle, -maxVerticalAngle, 0);
+            _mouseVerticalValue = verticalAngle;
+            
+        }
+    }
+
+    public float sensitivity;
+
+    private void Start()
+    {
+        cam_offset = cameraTransform.localPosition;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(Physics.Linecast(transform.position,transform.position+  transform.localRotation*cam_offset,out hit))
+        {
+            cameraTransform.localPosition = new Vector3(0, 0, -Vector3.Distance(transform.position, hit.point));
+        }
+        else
+        {
+            cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, cam_offset, Time.deltaTime);
+        }
+
+    MouseVerticalValue = Input.GetAxis("Mouse Y");
+
+        Quaternion finalRotation = Quaternion.Euler(
+            -MouseVerticalValue * sensitivity,
+        0, 0);
+
+        cameraTransform.localRotation = finalRotation;
+
+		body.rotation = Quaternion.Euler(
+	    0,
+	    body.localRotation.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivity,
+		0);
+
+		if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+
+}
---
+++ Assets/Game/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+	public float speed = 10;
+	public float jumpHeight = 10;
+	public PhysicalCC physicalCC;
+
+	public Transform bodyRender;
+
+	void Update()
+	{
+		if (physicalCC.isGround)
+		{
+			physicalCC.moveInput = Vector3.ClampMagnitude(transform.forward
+							* Input.GetAxis("Vertical")
+							+ transform.right
+							* Input.GetAxis("Horizontal"), 1f) * speed;
+
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				physicalCC.inertiaVelocity.y = 0f;
+				physicalCC.inertiaVelocity.y += jumpHeight;
+			}
+		}
+	}
+}
